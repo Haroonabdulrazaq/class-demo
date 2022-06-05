bbcd1b2b30d483a43196e7f8c2ede4fa752ce174
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2022-06-05 13:00:41 +0000

    Add description to todos table

--- todo/app.py
+++ todo/app.py
@@ -17,21 +17,22 @@ class Todo(db.Model):
      def __repr__(self):
         return f'<Todo ID: {self.id}, name: {self.description}>'
 
-
 db.create_all()
 
-@app.route("/todo/create", method=["POST"])
-def create():
-  description = request.form.get('description')
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return redirect(url_for('index'))
+
+@app.route('/todos/create', methods=['POST', 'GET']) 
+def create_todo():
+  if request.method == 'POST':
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+  else:
+     user = request.args.get('description')
+     return render_template('index.html')
 
 @app.route("/")
 def index():
-    # print('I am here' + Todo.query.all())
     return render_template('index.html', data=Todo.query.all())
-    # todo = Todo.query.first()
-    # print(todo.name)
-    # return "Hello " + todo.name
+
